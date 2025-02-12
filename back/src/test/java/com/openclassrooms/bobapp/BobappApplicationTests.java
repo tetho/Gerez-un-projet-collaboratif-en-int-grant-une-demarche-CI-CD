0d192d5552d2abf423a3f6871095694c594722d0
André Kouamé <kouameandre43@gmail.com>
2025-02-12 20:49:08 +0100

    failed test

--- back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
+++ back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
@@ -1,5 +1,6 @@
 package com.openclassrooms.bobapp;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class BobappApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals(0,1);
 	}
 
 }
