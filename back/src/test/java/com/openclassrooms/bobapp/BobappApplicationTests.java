07ebb930fe716f61b2e2bfb012eb393215b92d3a
Jerome <jerome.s42@hotmail.fr>
2025-02-12 17:43:19 +0100

    Test back-end ko

--- back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
+++ back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.bobapp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,8 @@ class BobappApplicationTests {
 	void contextLoads() {
 	}
 
+	 @Test
+	 public void testKO() {
+	    assertEquals(0, 1, "KO");
+	 }
 }
